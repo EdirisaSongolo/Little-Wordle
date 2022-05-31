2ddccb9651987ce7e43204ee74ad2f14ee366d85
Edrissongolo <Edrissongolo@Gmail.com>
2022-05-31 12:16:34 -0400

    Simpler algorithm

--- com/company/Main.java
+++ com/company/Main.java
@@ -1,13 +1,9 @@
 package com.company;
 
-
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
-	 // The solution colors will be represented using colors: G - Green, Y - Yellow, B - Black. The same rules apply as they do in real game.
+	 // The solution colors will be represented using colors: G - Green, Y - Yellow, B - Black. The same rules apply as they do in the real game.
     String [] user_grade = gradeUserInput("angry");
     System.out.println(String.join("", user_grade));
 
@@ -16,15 +12,13 @@ public class Main {
     public static String [] gradeUserInput (String user_input) {
         // Word of the day
         String word = "WEARY";
-
-        List<String> char_list = Arrays.asList(word.split(""));
-        String [] answer = user_input.toUpperCase().split("");
+        String answer = user_input.toUpperCase();
         String [] solution_response = new String [word.length()];
 
-        for (int index = 0; index < answer.length; index++) {
+        for (int index = 0; index < answer.length(); index++) {
 
-            if (char_list.contains(answer[index])) {
-             solution_response[index] = ( char_list.get(index).equals(answer[index]) ) ? "G" : "Y" ;
+            if (word.contains(String.valueOf(answer.charAt(index)))) {
+             solution_response[index] = ( word.charAt(index) == answer.charAt(index) ) ? "G" : "Y" ;
             } else {
               solution_response[index] = "B";
             }
